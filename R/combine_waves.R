435fc691f7fae322f603e7797a0c83b703170979
Damon Morris <62291572+djmorris1989@users.noreply.github.com>
2020-09-16 23:42:52 +0100

    sort data by id and wave, reorder columns

--- R/combine_waves.R
+++ R/combine_waves.R
@@ -3,25 +3,23 @@
 #' Combines years of data when provided as a list of data tables.
 #'
 #' @param data_list List of data tables to combine.
+#' @param ukhls TRUE if `data_list` contains UKHLS waves
 #'
-#' @return Returns data table of combined data.
-#' @export
-#'
-#' @examples
-#'
-#' \dontrun{
-#'
-#' data_all <- combine_years(list(data1, data2, data3))
-#'
-#' }
 #'
 combine_waves <- function(
-  data_list
+  data_list,
+  ukhls = TRUE
 ) {
 
   data <- data.table::rbindlist(data_list, use.names = T, fill = T)
 
-
+  if (ukhls == TRUE) {
+  data <- data[order(pid,pidp,wave_no),]
+  setcolorder(data, c("pid", "pidp", "wave_no"))
+  } else {
+  data <- data[order(pid,pidp,wave_no),]
+  setcolorder(data, c("pid", "wave_no"))
+  }
   # create a year-quarter time variable
   ##data$time <- as.yearqtr(paste0(data$year, "-", data$quarter))
 
