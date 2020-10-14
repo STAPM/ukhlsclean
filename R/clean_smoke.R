a253ba01757ead0e3d5c17d23fa3239f1c3e7902
Damon Morris <d.j.morris@sheffield.ac.uk>
2020-10-14 10:55:08 +0100

    create a measure of other smokers in the household

--- R/clean_smoke.R
+++ R/clean_smoke.R
@@ -32,11 +32,6 @@ clean_smoke <- function(data = NULL) {
   data[wave %in% c("UKHLS Wave 2","UKHLS Wave 5") & smever == 2, ever_smoked := "no"]
   data[wave %in% c("UKHLS Wave 2","UKHLS Wave 5") & smever == 1, ever_smoked := "yes"]
 
-  # make factors
-  data$non_smoker   <- as.factor(data$non_smoker)
-  data$ever_smoked  <- as.factor(data$ever_smoked)
-  data$current_smoker <- as.factor(data$current_smoker)
-
   # age started smoking
   data[smagbg == 0, smagbg := NA]
   data[, smk_age_start := pmin(smagbg,na.rm=TRUE), by = "id"]
@@ -44,8 +39,25 @@ clean_smoke <- function(data = NULL) {
   data[, smk_age_start := nafill(smk_age_start, type = "nocb"), by = "id"]
   data[, smk_age_start := nafill(smk_age_start, type = "locf"), by = "id"]
 
+  # indicator for any other household smokers
+  data[!(wave %in% c("UKHLS Wave 1","UKHLS Wave 3","UKHLS Wave 4")) & current_smoker == "yes", smoke := 1]
+  data[!(wave %in% c("UKHLS Wave 1","UKHLS Wave 3","UKHLS Wave 4")) & current_smoker == "no", smoke := 0]
+  data[!(wave %in% c("UKHLS Wave 1","UKHLS Wave 3","UKHLS Wave 4")), num_smoker_hhold := sum(smoke, na.rm=TRUE), by = c("wave","hidp")]
+      # number of other smokers = number of smokers - respondent
+  data[!(wave %in% c("UKHLS Wave 1","UKHLS Wave 3","UKHLS Wave 4")), num_othersmoker_hhold := num_smoker_hhold - smoke]
+      # create a binary indicator
+  data[!(wave %in% c("UKHLS Wave 1","UKHLS Wave 3","UKHLS Wave 4")) & num_othersmoker_hhold > 0, othersmoker_hhold := "yes"]
+  data[!(wave %in% c("UKHLS Wave 1","UKHLS Wave 3","UKHLS Wave 4")) & num_othersmoker_hhold == 0, othersmoker_hhold := "no"]
+
+
+  # make factors
+  data$non_smoker   <- as.factor(data$non_smoker)
+  data$ever_smoked  <- as.factor(data$ever_smoked)
+  data$current_smoker <- as.factor(data$current_smoker)
+  data$othersmoker_hhold <- as.factor(data$othersmoker_hhold)
+
   # remove raw variables no longer needed
-  data <- subset(data,select = -c(smever,smnow,smcigs,smncigs,aglquit,smagbg,smoker))
+  data <- subset(data,select = -c(smever,smnow,smcigs,smncigs,aglquit,smagbg,smoker,smoke,num_smoker_hhold,num_othersmoker_hhold))
 
 
   return(data)
