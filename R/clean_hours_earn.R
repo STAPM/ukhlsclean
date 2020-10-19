98b8c27873117d0e7e5b8fb8dcb979794a78f9b4
Damon Morris <cm1djm@sheffield.ac.uk>
2020-10-19 21:50:11 +0100

    bug fix

--- R/clean_hours_earn.R
+++ R/clean_hours_earn.R
@@ -1,7 +1,6 @@
 #' Clean Hours and Earnings Variables
 #'
-#' Reads and does basic cleaning on the UKHLS demographic variables -
-#' age, gender, ethnicity, region and urban vs rural.
+#' cleans earnings variables and converts into real terms.
 #'
 #' @export
 clean_hours_earn <- function(data = NULL) {
@@ -12,12 +11,12 @@ clean_hours_earn <- function(data = NULL) {
                             ukhlsclean::cpi,
                             by = c("year","month"),
                             all.x = TRUE)
-  ### marital status
+  ###
   merge[ , real_grss_pay_usual := grss_pay_usual*(100/cpi_value)]
   merge[ , real_grss_pay_last := grss_pay_last*(100/cpi_value)]
 
 
-  data <- subset(data,select = -c(grss_lab_inc))
+  merge <- subset(merge,select = -c(grss_lab_inc,cpi_value))
 
-  return(data)
+  return(merge)
 }
