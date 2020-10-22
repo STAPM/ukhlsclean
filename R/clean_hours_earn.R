7318b2098b5e3613bd29c61c75b93ed64f357951
Damon Morris <cm1djm@sheffield.ac.uk>
2020-10-22 14:03:10 +0100

    clean gross labour income variable and retain in the data

--- R/clean_hours_earn.R
+++ R/clean_hours_earn.R
@@ -11,12 +11,17 @@ clean_hours_earn <- function(data = NULL) {
                             ukhlsclean::cpi,
                             by = c("year","month"),
                             all.x = TRUE)
-  ###
+
+  # deal with negative values for gross labour income
+  merge[grss_lab_inc <= 0, grss_lab_inc := NA]
+
+  # construct real terms variables
   merge[ , real_grss_pay_usual := grss_pay_usual*(100/cpi_value)]
   merge[ , real_grss_pay_last := grss_pay_last*(100/cpi_value)]
+  merge[ , real_grss_lab_inc := grss_lab_inc*(100/cpi_value)]
 
 
-  merge <- subset(merge,select = -c(grss_lab_inc,cpi_value))
+  merge <- subset(merge,select = -c(cpi_value))
 
   return(merge)
 }
