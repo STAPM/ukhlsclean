1db1292bbf6d6f7b1b0f4a4401cc1cbed98c1842
djmorris1989 <d.j.morris@sheffield.ac.uk>
2022-03-01 14:02:09 +0000

    update the global cleaning function

--- R/ukhls_clean_global.R
+++ R/ukhls_clean_global.R
@@ -6,39 +6,41 @@
 #'
 #' @param data Data table - the combined Understanding Society dataset.
 #' @param ages Integer vector - the ages in single years to retain (defaults to 16 to 89 years).
+#' @param waves Integer vector - the waves of the UKHLS to retain (defaults to all - 1 to 11).
 #' @param keep_vars Character vector - the names of the variables to keep (defaults to NULL - retaining all variables).
 #' @param complete_vars Character vector - the names of the variables on which the selection of complete cases will be based (defaults to year, age and gender).
 #' @return Returns a new set of variables
 #' @export
 ukhls_clean_global <- function(data,
                                ages = 16:89,
+                               waves = 1:11,
                                keep_vars = NULL,
-                               complete_vars = c("year", "age", "gender")
+                               complete_vars = c("year", "age", "sex")
 ) {
 
-  data <- data %>%
-    clean_demographic() %>%
-    clean_health() %>%
-    clean_alcohol() %>%
-    clean_smoke() %>%
-    clean_econ_status() %>%
-    clean_hhold() %>%
-    clean_hours_earn()
+  ## fix bug that occurs if age is not in keep_vars
 
-  ## if keep_vars argument is NULL, keep all variables
-  if (is.null(keep_vars)) {
-  keep_vars <- names(data)
-  } else if (!is.null(keep_vars))
-  keep_vars <- intersect(names(data), keep_vars)
-
-  data <- data[ , keep_vars, with = F]
-
-  ## only keep cases where all variables in complete_vars are non-missing
-  for(cv in complete_vars) {
-    data <- data[!is.na(get(cv))]
+  if (!("age" %in% names(data)) ) {
+    ages = NULL
   }
 
-  data <- data[age %in% ages]
+  ## run the cleaning functions
+
+  data %<>%
+    clean_demographic %>%
+    clean_health %>%
+    clean_alcohol %>%
+    clean_smoke %>%
+    clean_econ_status %>%
+    clean_hhold %>%
+    clean_hours_earn %>%
+
+  ukhlsclean::select_data(
+    ages = ages,
+    waves = waves,
+    keep_vars = keep_vars,
+    complete_vars = complete_vars
+  )
 
 
   return(data)
