4d8cd27007fd61b3af205491c1580a7196741e42
Damon Morris <d.j.morris@sheffield.ac.uk>
2022-03-02 13:58:04 +0000

    update CPI data to December 2021

--- data-raw/cpi.R
+++ data-raw/cpi.R
@@ -1,10 +1,11 @@
 library(readxl)
+library(data.table)
 
 ## read in the downloaded cpi index data
 
-data <- read_excel(path = paste0("data-raw/","CPI Index 19-06-2020",".xls"),
+data <- read_excel(path = paste0("data-raw/","CPI Index 02-03-2022",".xls"),
                    sheet = "data",
-                   range = "B43:B393",
+                   range = "B43:B414",
                    col_names = FALSE)
 
 cpi_value <- as.vector(as.matrix(data))
@@ -13,9 +14,9 @@ cpi_value <- as.numeric(cpi_value)
 ### create a year and month indicator from Jan 2004 to December of the
 ### current year of APS data
 
-year <- c(rep(1991:2019,each=12),2020,2020,2020)
+year <- c(rep(1991:2021,each=12))
 
-month <- c(rep(seq(1:12),2019-1991+1),1,2,3)
+month <- c(rep(seq(1:12),2021-1991+1))
 
 cpi <- matrix(c(year,month,cpi_value),
               byrow=FALSE,
@@ -28,5 +29,8 @@ cpi[,"cpi_value"] <- 100*(cpi[,"cpi_value"]/(cpi[length(cpi[,"cpi_value"]),"cpi_
 print(paste0("Base Year = ",cpi[length(cpi[,"year"]),"year"]))
 print(paste0("Base Month = ",cpi[length(cpi[,"month"]),"month"]))
 
-usethis::use_data(cpi,overwrite=TRUE)
+cpi <- as.data.table(cpi)
 
+usethis::use_data(cpi, overwrite=TRUE)
+
+rm(data, cpi, cpi_value, month, year)
