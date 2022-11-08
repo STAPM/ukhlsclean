5ea1defb3a55348aa08c30a1dbd5821d3ade2454
Damon Morris <d.j.morris@sheffield.ac.uk>
2022-11-08 15:37:41 +0000

    update smoking module

--- R/ukhls_clean_smoke.R
+++ R/ukhls_clean_smoke.R
@@ -8,78 +8,54 @@
 #' @export
 ukhls_clean_smoke <- function(data = NULL) {
 
-  #cat(crayon::green("\tCleaning smoking variables\n"))
-
-  # ensure data is sorted
   data <- data[order(id,wave_no),]
+
+  #####################################################
+  #### Waves 2 and 5 - smoking status, ever smoked ####
 
   if ("smever" %in% colnames(data)) {
 
-  ########################
-  ### non-smoking status #
+  ## current smoker status
+  data[wave_no %in% c(2,5) & smever == 1 & smnow == 1, current_smoker := "smoker"]
+  data[wave_no %in% c(2,5) & smever == 1 & smnow == 2, current_smoker := "non_smoker"]
+  data[wave_no %in% c(2,5) & smever == 2, current_smoker := "non_smoker"]
 
-  data[wave_no %in% c(2,5) & smever == 2, non_smoker := "yes"]
-  data[wave_no %in% c(2,5) & smever == 1 & smnow == 2, non_smoker := "yes"]
-  data[wave_no %in% c(2,5) & smnow == 1, non_smoker := "no"]
+  ## ever smoked
+  data[wave_no %in% c(2,5) & smever == 2, ever_smoked := "never_smoked"]
+  data[wave_no %in% c(2,5) & smever == 1, ever_smoked := "smoked"]
 
-  ###########################
-  ## current smoker status ##
-  data[wave_no %in% c(2,5) & smever == 1 & smnow == 1, current_smoker := "yes"]
-  data[wave_no %in% c(2,5) & smever == 1 & smnow == 2, current_smoker := "no"]
-  data[wave_no %in% c(2,5) & smever == 2, current_smoker := "no"]
-
-  ##################
-  ## ever smoked ###
-  data[wave_no %in% c(2,5) & smever == 2, ever_smoked := "no"]
-  data[wave_no %in% c(2,5) & smever == 1, ever_smoked := "yes"]
-
-
-
-  data[, non_smoker   := as.factor(data$non_smoker)]
-  data[, ever_smoked  := as.factor(data$ever_smoked)]
-  data[, current_smoker := as.factor(data$current_smoker)]
+  data[, ever_smoked  := as.factor(ever_smoked)]
+  data[, current_smoker := as.factor(current_smoker)]
 
   } else {
 
   data[, ever_smoked := NA]
   }
 
-
+  ###########################################
+  ##### Waves 6 - 11 current smoker status ##
 
   if ("smoker" %in% colnames(data)) {
 
-  ##########################
-  # current smoker status ##
+  data[wave_no %in% c(6:11) & smoker == 1, current_smoker := "smoker"]
+  data[wave_no %in% c(6:11) & smoker == 2, current_smoker := "non_smoker"]
 
-  data[wave_no %in% c(6:11) & smoker == 1, current_smoker := "yes"]
-  data[wave_no %in% c(6:11) & smoker == 2, current_smoker := "no"]
-
-  #######################
-  # non-smoker status ###
-
-  data[wave_no %in% c(6:11) & smoker == 1, non_smoker := "no"]
-  data[wave_no %in% c(6:11) & smoker == 2, non_smoker := "yes"]
-
-  data[, c("smoker") := NULL]
+  data[, current_smoker := as.factor(current_smoker)]
 
   }
 
-  if (!("smoker" %in% colnames(data)) & !("smever" %in% colnames(data)) ){
+  if (!("current_smoker" %in% colnames(data)) ){
 
   data[, current_smoker := NA]
-  data[, non_smoker := NA]
   }
 
-  if ("smagbg" %in% colnames(data)) {
+  ########################################
+  # age started smoking (waves 2 and 5) ##
 
-  ########################
-  # age started smoking ##
+  if ("smagbg" %in% colnames(data)){
 
   data[smagbg == 0, smagbg := NA]
-  data[, smk_age_start := pmin(smagbg,na.rm=TRUE), by = "id"]
-
-  data[, smk_age_start := nafill(smk_age_start, type = "nocb"), by = "id"]
-  data[, smk_age_start := nafill(smk_age_start, type = "locf"), by = "id"]
+  data[, smk_age_start := smagbg]
 
   data[, c("smagbg") := NULL]
   } else {
@@ -87,11 +63,14 @@ ukhls_clean_smoke <- function(data = NULL) {
   data[, smk_age_start := NA]
   }
 
+  #################################################################################
+  #### Other smokers in household - waves 2, 5-11 (derived from current_smoker ####
+
   if ("current_smoker" %in% colnames(data)) {
 
   # indicator for any other household smokers
-  data[!(wave_no %in% c(1,3,4)) & current_smoker == "yes", smoke := 1]
-  data[!(wave_no %in% c(1,3,4)) & current_smoker == "no", smoke := 0]
+  data[!(wave_no %in% c(1,3,4)) & current_smoker == "smoker", smoke := 1]
+  data[!(wave_no %in% c(1,3,4)) & current_smoker == "non_smoker", smoke := 0]
   data[!(wave_no %in% c(1,3,4)), num_smoker_hhold := sum(smoke, na.rm=TRUE), by = c("wave","hidp")]
       # number of other smokers = number of smokers - respondent
   data[!(wave_no %in% c(1,3,4)), num_othersmoker_hhold := num_smoker_hhold - smoke]
@@ -99,7 +78,7 @@ ukhls_clean_smoke <- function(data = NULL) {
   data[!(wave_no %in% c(1,3,4)) & num_othersmoker_hhold > 0, othersmoker_hhold := "yes"]
   data[!(wave_no %in% c(1,3,4)) & num_othersmoker_hhold == 0, othersmoker_hhold := "no"]
 
-  data[, othersmoker_hhold := as.factor(data$othersmoker_hhold)]
+  data[, othersmoker_hhold := as.factor(othersmoker_hhold)]
 
   } else {
 
@@ -111,11 +90,9 @@ ukhls_clean_smoke <- function(data = NULL) {
   ## RETAIN THE CLEANED VARIABLES
 
   final_data <- data[, c("id", "hidp", "wave_no",
-                         "current_smoker", "ever_smoked", "non_smoker", "smk_age_start",
-                         "othersmoker_hhold")]
+                         "current_smoker", "ever_smoked", "smk_age_start", "othersmoker_hhold")]
 
-  var_names <- c("current_smoker", "ever_smoked", "non_smoker", "smk_age_start",
-                 "othersmoker_hhold")
+  var_names <- c("current_smoker", "ever_smoked", "smk_age_start", "othersmoker_hhold")
 
   setnames(final_data, var_names, paste0("s_", var_names))
 
