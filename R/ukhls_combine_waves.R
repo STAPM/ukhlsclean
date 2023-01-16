f5c7b3d5a9668a6ac670e650028da79d79ab118a
Damon Morris <d.j.morris@sheffield.ac.uk>
2023-01-16 10:21:46 +0000

    add wave 12 identifier variable

--- R/ukhls_combine_waves.R
+++ R/ukhls_combine_waves.R
@@ -31,6 +31,7 @@ ukhls_combine_waves <- function(
   data[, wave_9  := ifelse(wave_no == 9,1,0)]
   data[, wave_10 := ifelse(wave_no == 10,1,0)]
   data[, wave_11 := ifelse(wave_no == 11,1,0)]
+  data[, wave_12 := ifelse(wave_no == 12,1,0)]
 
   data[, wave_1  := max(wave_1) , by = "id"]
   data[, wave_2  := max(wave_2) , by = "id"]
@@ -43,6 +44,7 @@ ukhls_combine_waves <- function(
   data[, wave_9  := max(wave_9) , by = "id"]
   data[, wave_10 := max(wave_10), by = "id"]
   data[, wave_11 := max(wave_11), by = "id"]
+  data[, wave_12 := max(wave_12), by = "id"]
 
   ## calculate number of waves each individual is in
 
@@ -51,7 +53,7 @@ ukhls_combine_waves <- function(
   # order columns
   setcolorder(data, c("id","hidp","wave_no","bhps_sample","nwaves",
                       "wave_1","wave_2","wave_3","wave_4","wave_5","wave_6",
-                      "wave_7","wave_8","wave_9","wave_10","wave_11"))
+                      "wave_7","wave_8","wave_9","wave_10","wave_11","wave_12"))
 
 
   return(data)
